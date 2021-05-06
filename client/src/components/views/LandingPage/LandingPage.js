ef8f0501394c0d850ed1f25a2c0307fca7bc46d3
J.S <pachipico1@gmail.com>
2021-05-07 01:06:02 +0900

    add needs-to-be-fixed things in chart.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,30 +5,56 @@ import { Row, Col, Card, List, Avatar } from "antd";
 const Meta = Card;
 
 function LandingPage() {
+	const [Date, setDate] = useState("");
 	const [Chart, setChart] = useState([]);
 	useEffect(() => {
 		Axios.get("/api/chart/getChart").then((response) => {
 			setChart(response.data.chart);
 		});
 	}, []);
-
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let variable = { date: Date };
+		Axios.post("/api/chart/specificDateChart", variable).then((response) => {
+			if (response.data.success) {
+				setChart(response.data.chart);
+			} else {
+				alert("failed to get chart");
+			}
+		});
+		setDate("");
+	};
+	const handleDateChange = (e) => {
+		setDate(e.target.value);
+	};
 	return (
-		<List
-			style={{ marginLeft: "5%" }}
-			itemLayout='horizontal'
-			dataSource={Chart}
-			renderItem={(item) => (
-				<List.Item>
-					<span style={{ position: "absolute" }}>{item.rank}</span>
-					<List.Item.Meta
-						style={{ paddingLeft: "40px" }}
-						avatar={<Avatar shape='square' size='large' src={item.cover} />}
-						title={<span>{item.title}</span>}
-						description={item.artist}
-					/>
-				</List.Item>
-			)}
-		/>
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='date'
+					name='date'
+					value={Date}
+					onChange={handleDateChange}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
+			<List
+				style={{ marginLeft: "5%" }}
+				itemLayout='horizontal'
+				dataSource={Chart}
+				renderItem={(item) => (
+					<List.Item>
+						<span style={{ position: "absolute" }}>{item.rank}</span>
+						<List.Item.Meta
+							style={{ paddingLeft: "40px" }}
+							avatar={<Avatar shape='square' size='large' src={item.cover} />}
+							title={<span>{item.title}</span>}
+							description={item.artist}
+						/>
+					</List.Item>
+				)}
+			/>
+		</div>
 	);
 }
 export default LandingPage;
--- server/routes/chart.js
+++ server/routes/chart.js
@@ -22,4 +22,16 @@ router.get("/getChart", (req, res) => {
 	});
 });
 
+router.post("/specificDateChart", (req, res) => {
+	getChart("hot-100", req.body.date, (err, chart) => {
+		if (err) res.status(400).json({ success: false, err });
+		else res.status(200).json({ success: true, chart: chart.songs });
+	});
+});
+
 module.exports = router;
+// 날짜 값을 입력받아 입력한 날짜 당시의 차트 렌더링되게.
+// 1~10위권은 부각되게? 1~10위 버튼, 11~20위 버튼 ...
+//날짜 입력란 고칠것
+// a태그로 감싸서 유투브에 `${title}-${artist}`식으로 검색할수있게 만들것
+// 가수 || 곡 제목으로 검색해 빌보드 히스토리 보여주기.
